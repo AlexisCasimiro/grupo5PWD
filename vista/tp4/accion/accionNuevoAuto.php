c2a369a14079cf48f7ed34566e74a3cd940ef015
Alexis Casimiro <exequielcasimiro@gmail.com>
2023-09-15 11:37:21 -0300

    terminando punto 5

--- vista/tp4/accion/accionNuevoAuto.php
+++ vista/tp4/accion/accionNuevoAuto.php
@@ -3,23 +3,33 @@
     include_once("../../../configuracion.php");
     //obtengo los datos del formulario
     $datos= data_submitted();
-    //test borrar despues
-    print_r($datos);
-    echo $datos['DniDuenio'];
-    //
     //busco una persona con el dni ingresado 
     $objAbmPersona= new AbmPersona;
-    $dni=$datos['DniDuenio'];
+    //creo el arreglo con DniDuenio para buscar()
     $dniDuenio['NroDni']=$datos['DniDuenio'];
-    
-    $array=$objAbmPersona->buscar($dniDuenio);
-    
-    print_r($array);
-    if($array == []){
-        echo "<div class='alert alert-danger' role='alert'>No hay ninguna persona cargada con el número de D.N.I:".$datos['DniDuenio']."</div>";
-        echo "<div class='alert alert-success' role='alert'><a href='../nuevaPersona.php'>Agregar una nueva Persona</div>";
-    }else{
-        echo "existe";
+    $arrayPersona=$objAbmPersona->buscar($dniDuenio);
+    $existe=true;
+    if($arrayPersona == []){
+        echo "<div class='alert alert-danger' role='alert'>No hay ninguna persona cargada con el número de D.N.I: ".$datos['DniDuenio']."</div>";
+        echo "<div class='alert alert-primary' role='alert'><a href='../nuevaPersona.php'>Agregar una nueva Persona</a></div>";
+        $existe=false;
+    }
+    $objAbmAuto= new AbmAuto;
+    $patente['patente']=$datos['Patente'];
+    $arrayAuto=$objAbmAuto->buscar($patente);
+    if($arrayAuto != []){
+        echo "<div class='alert alert-danger' role='alert'>Ya hay un auto cargado con la patente: ".$datos['Patente']."</div>";
+        $existe=false;
+    }
+
+    if($existe){
+        $objAbmAutoNuevo=new AbmAuto;
+        $alta=$objAbmAutoNuevo->alta($datos);
+        if($alta){
+            echo "<div class='alert alert-success' role='alert'>Se ingreso Correctamente el auto a la Base de Datos</div>";
+        }else{
+            echo "<div class='alert alert-danger' role='alert'>No se pudo Ingresar el auto a la base de datos</div>";
+        }
     }
 ?>
 
