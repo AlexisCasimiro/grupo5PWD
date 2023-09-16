e007009d48d9a298ea0cc8e873fd58c67aad2c32
Matias-Ignacio <matias.riveiro@est.fi.uncoma.edu.ar>
2023-09-15 21:46:51 -0300

    Ej7 accionBuscarPersona

--- vista/tp4/actualizarDatosPersona.php
+++ vista/tp4/actualizarDatosPersona.php
@@ -1,32 +1,19 @@
 <?php
     $Titulo = "Actualizar";
     include_once("../estructura/header.php");
-$datos = (data_submitted());
+
 
 $datos = data_submitted();
 $objAbmPersona = new AbmPersona();
 //$objPersona = NULL;
 // var_dump($datos);
-?>
-        <?php
-        
-            if (isset($datos['NroDni'])){
-                $dniPersona["NroDni"] = $datos["NroDni"];
 
-                
-                $array = $objAbmPersona->buscar($dniPersona);
-                if (count($array)==0){
 
-                        $Resultado = $objAbmPersona->alta($datos);
-                        echo "<div class='alert alert-success' role='alert'>Se agregó correctamente la Persona a la Base de Datos</div>";
-                }else{
-                    echo "<div class='alert alert-danger' role='alert'>La persona ya se encuentra en la Base de Datos</div>";
-                }
-            }
-            
-        ?>
-<?php
-?>
+
+
+
+
+
 <?php
 include_once("../../estructura/footer.php");
 ?>
--- vista/tp4/buscarPersona.php
+++ vista/tp4/buscarPersona.php
@@ -10,7 +10,7 @@ include_once("../estructura/header.php");
 <form class="container pt-3" action="./accion/accionBuscarPersona.php" method="post" name="formBuscarPersona" id="formBuscarPersona">
     <div class="form-group row align-items-center">
         <label for="numDoc" class="col" style="max-width: fit-content">Numero de documento:</label>
-        <input type="number" class="form-control col" name="dni" id="dni" placeholder="Ingrese el numero de documento" style="max-width: 280px; max-height:30px;">
+        <input type="number" class="form-control col" name="NroDni" id="NroDni" placeholder="Ingrese el numero de documento" style="max-width: 280px; max-height:30px;">
         <button type="submit" class="btn btn-success btn-sm m-2 enviar" name="enviar" id="enviar">Buscar</button>
     </div>
 </form>
