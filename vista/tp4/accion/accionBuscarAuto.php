13cc622b336bb1a9b6d8eb198c715f85b9948208
Matias-Ignacio <116597566+Matias-Ignacio@users.noreply.github.com>
2023-09-17 13:15:46 -0300

    Update accionBuscarAuto.php

--- vista/tp4/accion/accionBuscarAuto.php
+++ vista/tp4/accion/accionBuscarAuto.php
@@ -4,18 +4,22 @@
 
     $datos = data_submitted();
     $objAbmAuto = new AbmAuto();
-    $objAuto =NULL;
+    $objAuto = null;
+    $objDuenio = null;
     if (isset($datos['patente'])){
 
         $listaTabla = $objAbmAuto->buscar($datos);
         if (count($listaTabla)==1){
             $objAuto= $listaTabla[0];
+            $objDuenio = $objAuto->getobjPersona();
         }
     }
 ?>
 <div class="container mt-3">
   <h2 style="text-align: center; color:dodgerblue;">Buscar Auto por la Patente</h2>
   <h5 style="text-align: center; color:dodgerblue;">Auto registrado en la base de datos</h5>            
+<?php if ($objAuto!=null){
+?>
   <table class="table">
     <thead>
       <tr><th>Patente</th>
@@ -24,17 +28,42 @@
         </tr>
     </thead>
     <tbody>
-<?php if ($objAuto!=null){
-    echo '<tr><td style="width:200px;">'.$objAuto->getPatente().'</td>';
-	echo '<td style="width:200px;">'.$objAuto->getMarca().'</td>';
-    echo '<td style="width:200px;">'.$objAuto->getModelo().'</td>';
-
- }else {
-    
-    echo '<h5 style="text-align: left; color:red;">No se encontro la Patente solicitada</5>';
-}?>
+        <tr>
+<?php            
+        echo '<td style="width:200px;">'.$objAuto->getPatente().'</td>';
+        echo '<td style="width:200px;">'. $objAuto->getMarca().' </td>';
+        echo '<td style="width:200px;">'.  $objAuto->getModelo().' </td>';       
+?>
+        </tr>       
     </tbody>
-  </table>
+ </table>
+ <h4 style="text-align: center; color:dodgerblue;">Datos del Titular</h4> 
+ <table class="table">
+    <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Dni Titular</th>
+            <th>Telefono</th>
+            <th>Domicilio</th>
+        </tr>
+    </thead>
+        <tbody>
+            <tr>
+<?php            
+        echo '<td style="width:200px;">'.$objDuenio->getNombre().'</td>';
+        echo '<td style="width:200px;">'. $objDuenio->getApellido().' </td>';
+        echo '<td style="width:200px;">'. $objDuenio->getDni().' </td> ';
+        echo '<td style="width:200px;">'.  $objDuenio->getTelefono().' </td>';
+        echo '<td style="width:200px;">'. $objDuenio->getDomicilio().' </td> ';
+?>
+            </tr>       
+        </tbody>
+    </table>        
+<?php        
+ }else {
+    echo '<h5 style="text-align: left; color:red;">No se encontro la Patente solicitada</h5><br>';
+}?>
 	<a href="../buscarAuto.php" class="btn btn-primary">Volver</a>
 </div>
 
