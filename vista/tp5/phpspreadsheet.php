680237402d516e7bf7bbdd5a16bb3e0dc0c8474c
UrraPablo <94190805+UrraPablo@users.noreply.github.com>
2023-10-03 00:06:24 -0300

    creacion de modelo-control

--- vista/tp5/phpspreadsheet.php
+++ vista/tp5/phpspreadsheet.php
@@ -5,11 +5,11 @@ require '../../vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
-$spreadsheet = new Spreadsheet();
-$activeWorksheet = $spreadsheet->getActiveSheet();
-$activeWorksheet->setCellValue('A1', 'Prueba escribir en A1 !');
+$spreadsheet = new Spreadsheet(); // crea un obj spreadsheet 
+$activeWorksheet = $spreadsheet->getActiveSheet(); // llama al metodo para activar el obj
+$activeWorksheet->setCellValue('A1', 'Prueba escribir en A1 !');// en la celda A1 , setea el valor pasado como parametro
 
-$writer = new Xlsx($spreadsheet);
+$writer = new Xlsx($spreadsheet); // crea un obj para escribir en el excel
 $writer->save('grupo5.xlsx');
 
 echo "<h1>Hecho</h1>";
